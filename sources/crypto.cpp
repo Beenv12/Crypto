f34ed24678cedc99ffb9c221706d43c318bbc854
Beenv12 <36257286+Beenv12@users.noreply.github.com>
2018-05-20 18:15:28 +0300

    Update crypto.cpp

--- sources/crypto.cpp
+++ sources/crypto.cpp
@@ -1,160 +1,158 @@
 #include <crypto.hpp>
-
+ 
 namespace HWCrypto
 {
-	Crypto::Crypto(){};
-	Crypto::Crypto(std::string word, std::string alphabet)
+Crypto::Crypto(){};
+Crypto::Crypto(std::string word, std::string alphabet)
+{
+	cryptoword = word;
+	cryptoalphabet = alphabet;
+};
+Crypto::~Crypto(){};
+void Crypto::MethodCaesar()
+{
+ 
+	int select = 0;
+	int key = 0;
+	std::cout << "1. Шифрование" << std::endl;
+	std::cout << "2. Дешифрование" << std::endl;
+	std::cin >> select;
+	while (select < 1 || select > 2)
 	{
-		cryptoword = word;
-		cryptoalphabet = alphabet;
-	};
-	Crypto::~Crypto() {};
-	void Crypto::MethodCaesar()
-	{
-
-		int select = 0;
-		int key = 0;
-		std::cout << "1. Шифрование" << std::endl;
-		std::cout << "2. Дешифрование" << std::endl;
+		std::cout << "Error: некорректное значение." << std::endl;
 		std::cin >> select;
-		 while (select<1 || select>2)
-		 {
-		   std::cout<<"Error: некорректное значение."<<std::endl;
-		   std::cin>>select;
-		 }
-		
-		// Ввод ключа. Если введен не корректно, то завершаем программу.
-		 std::cout << "Введите ключ" << std::endl;
-		 		 if (std::cin >> key)
-			 std::cout << "Ключ - ";
-		 else
-		 {
-			 std::cout << "Error: некорректное значение. Ключ состоит из целых чисел.";
-			 exit(0);
-		 }
-		 
-		 
-
-		 // Шифр Цезаря.
-		switch (select)
-		{
-		case 1:
-		{
-			for (int i = 0; i<cryptoword.size(); ++i)
-				for (int j = 0; j<cryptoalphabet.size(); ++j)
-					if (cryptoword[i] == cryptoalphabet[j])
-					{
-						cryptoword[i] = cryptoalphabet[(j + key) % cryptoalphabet.size()];
-						break;
-					}
-			std::cout << "Зашифрованное сообщение - " << cryptoword;
-			break;
-		}
-		case 2:
-		{	
-		for (int i = 0; i<cryptoword.size(); ++i)
-				for (int j = 0; j<cryptoalphabet.size(); ++j)
-					if (cryptoword[i] == cryptoalphabet[j])
-					{
-						cryptoword[i] = cryptoalphabet[(j - key) % cryptoalphabet.size()];
-						break;
-					}
-			std::cout << "Дешифрованное сообщение - " << cryptoword;
-			break;
-		}
-		}
 	}
-	void Crypto::MethodSlogan()
+ 
+	// Ввод ключа. Если введен не корректно, то завершаем программу.
+	std::cout << "Введите ключ" << std::endl;
+	if (std::cin >> key)
+		std::cout << "Ключ - ";
+	else
 	{
-
-		int select = 0;
-		std::string key="";
-		bool flag=0;
-		int keysymb=0;
-		int wordsymb=0;
-		std::cout << "1. Шифрование" << std::endl;
-		std::cout << "2. Дешифрование" << std::endl;
-		std::cin >> select;
-		while (select<1 || select>2)
-		{
-			std::cout << "Error: некорректное значение." << std::endl;
-			std::cin >> select;
-		}
-
-		// Ввод ключа. Если введен не корректно, то завершаем программу.
-		std::cout << "Введите ключ" << std::endl;
-		std::cin >> key;
-		std::cout<<key.size()<<std::endl;
-		if (CorrectInputData(key, cryptoalphabet) == 1)
-			std::cout << "Введенный ключ - "<<key<<std::endl;
-		else
-		{
-			std::cout << "Error: некорректное значение. Ключ состоит из символов алфавита."<<std::endl;
-			exit(0);
-		}
-		if (key.size()>cryptoword.size())
-                {
-                        std::cout<< "Error: некорректное значение. Ключ не может быть длинее слова."<<std::endl;
-	                exit(0);
-		}
-		
-		while (key.size() != cryptoword.size())
-		{
-			for (int i=0;i<cryptoalphabet.size();++i)
-			{
-				flag=0;
-				for (int j=0;j<key.size();++j)
-					if (key[j]==cryptoalphabet[i])
-					{
-						flag=1;
-						break;
-					}
-				if (flag!=1)
+		std::cout << "Error: некорректное значение. Ключ состоит из целых чисел.";
+		exit(0);
+	}
+ 
+	// Шифр Цезаря.
+	switch (select)
+	{
+	case 1:
+	{
+		for (int i = 0; i < cryptoword.size(); ++i)
+			for (int j = 0; j < cryptoalphabet.size(); ++j)
+				if (cryptoword[i] == cryptoalphabet[j])
 				{
-					key=key+cryptoalphabet[i];
+					cryptoword[i] = cryptoalphabet[(j + key) % cryptoalphabet.size()];
 					break;
 				}
-			}
-		}
-		std::cout << "Ключ лозунга - " << key << std::endl;
-
-		// Лозунговый шифр.
-		switch (select)
-		{
-		case 1:
-		{
-			for (int i = 0; i<cryptoword.size(); ++i)
-			{
-				for (int j = 0; j<cryptoalphabet.size(); ++j)
+		std::cout << "Зашифрованное сообщение - " << cryptoword;
+		break;
+	}
+	case 2:
+	{
+		for (int i = 0; i < cryptoword.size(); ++i)
+			for (int j = 0; j < cryptoalphabet.size(); ++j)
+				if (cryptoword[i] == cryptoalphabet[j])
 				{
-					if (cryptoword[i] == cryptoalphabet[j])
-						wordsymb=j;
-					else if (key[i]==cryptoalphabet[j])
-						keysymb=j;
+					cryptoword[i] = cryptoalphabet[(j - key) % cryptoalphabet.size()];
+					break;
 				}
-				cryptoword[i]=cryptoalphabet[(wordsymb+keysymb)%cryptoalphabet.size()];
-				wordsymb=0;
-				keysymb=0;
-			}							}
-			std::cout << "Зашифрованное сообщение - " << cryptoword;
-			break;
-		}
-		case 2:
-		{
-			std::cout << "Дешифрованное сообщение - " << cryptoword;	
-			break;
-		}
+		std::cout << "Дешифрованное сообщение - " << cryptoword;
+		break;
+	}
 	}
 }
-
-
+void Crypto::MethodSlogan()
+{
+ 
+	int select = 0;
+	std::string key = "";
+	bool flag = 0;
+	int keysymb = 0;
+	int wordsymb = 0;
+	std::cout << "1. Шифрование" << std::endl;
+	std::cout << "2. Дешифрование" << std::endl;
+	std::cin >> select;
+	while (select < 1 || select > 2)
+	{
+		std::cout << "Error: некорректное значение." << std::endl;
+		std::cin >> select;
+	}
+ 
+	// Ввод ключа. Если введен не корректно, то завершаем программу.
+	std::cout << "Введите ключ" << std::endl;
+	std::cin >> key;
+	std::cout << key.size() << std::endl;
+	if (CorrectInputData(key, cryptoalphabet) == 1)
+		std::cout << "Введенный ключ - " << key << std::endl;
+	else
+	{
+		std::cout << "Error: некорректное значение. Ключ состоит из символов алфавита." << std::endl;
+		exit(0);
+	}
+	if (key.size() > cryptoword.size())
+	{
+		std::cout << "Error: некорректное значение. Ключ не может быть длинее слова." << std::endl;
+		exit(0);
+	}
+ 
+	while (key.size() != cryptoword.size())
+	{
+		for (int i = 0; i < cryptoalphabet.size(); ++i)
+		{
+			flag = 0;
+			for (int j = 0; j < key.size(); ++j)
+				if (key[j] == cryptoalphabet[i])
+				{
+					flag = 1;
+					break;
+				}
+			if (flag != 1)
+			{
+				key = key + cryptoalphabet[i];
+				break;
+			}
+		}
+	}
+	std::cout << "Ключ лозунга - " << key << std::endl;
+ 
+	// Лозунговый шифр.
+	switch (select)
+	{
+	case 1:
+	{
+		for (int i = 0; i < cryptoword.size(); ++i)
+		{
+			for (int j = 0; j < cryptoalphabet.size(); ++j)
+			{
+				if (cryptoword[i] == cryptoalphabet[j])
+					wordsymb = j;
+				else if (key[i] == cryptoalphabet[j])
+					keysymb = j;
+			}
+			cryptoword[i] = cryptoalphabet[(wordsymb + keysymb) % cryptoalphabet.size()];
+			wordsymb = 0;
+			keysymb = 0;
+		}
+	}
+		std::cout << "Зашифрованное сообщение - " << cryptoword;
+		break;
+	}
+case 2:
+{
+	std::cout << "Дешифрованное сообщение - " << cryptoword;
+	break;
+}
+}
+}
+ 
 bool CorrectInputData(std::string word, std::string alphabet)
 {
 	bool correct = 0;
-	for (int i = 0; i<word.size(); ++i)
+	for (int i = 0; i < word.size(); ++i)
 	{
 		correct = 0;
-		for (int j = 0; j<alphabet.size(); ++j)
+		for (int j = 0; j < alphabet.size(); ++j)
 		{
 			if (alphabet[j] == word[i])
 			{
@@ -169,7 +167,7 @@ bool CorrectInputData(std::string word, std::string alphabet)
 	}
 	return 1;
 }
-
+ 
 int Interface::ChosenFunction()
 {
 	setlocale(LC_ALL, "Russian");
@@ -185,21 +183,21 @@ int Interface::ChosenFunction()
 	std::cin >> chosenValue;
 	return chosenValue;
 }
-
+ 
 void Interface::MakeDecision(int chosenValue, Crypto *crypto)
 {
 	switch (chosenValue)
 	{
-	case 1:
-	{
-		crypto->MethodCaesar();
-		break;
-	}
-	case 2:
-	{
-		crypto->MethodSlogan();
-		break;
-	}
+		case 1:
+		{
+			crypto->MethodCaesar();
+			break;
+		}
+		case 2:
+		{
+			crypto->MethodSlogan();
+			break;
+		}
 	}
 }
 }
