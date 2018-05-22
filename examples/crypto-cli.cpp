49a3ad06190435377ce9a01454b1d0da67c9671d
Beenv12 <36257286+Beenv12@users.noreply.github.com>
2018-05-22 13:51:13 +0300

    Update crypto-cli.cpp

--- examples/crypto-cli.cpp
+++ examples/crypto-cli.cpp
@@ -1,31 +1,29 @@
 #include <crypto.hpp>
 
-
-
 using namespace HWCrypto;
 
 int main(int argc, char *argv[])
 {
-	Interface obj;
-	if (argv[1] == 0)
-	{
-		std::cout << "Входных данных нет";
-		return 0;
-	}
-	std::string word = argv[1];
-	std::string alphabet = "abcdefghijklmnopqrstuvwxyz ";
-	int chosenValue;
-	if (CorrectInputData(word, alphabet) == 0)
-	{
-		std::cout << "Некорректный ввод данных. Вводите данные из следующего алфавита - 'abcdefghijklmnopqrstuvwxyz '. ";
-		return 0;
-	}
-	
-	Crypto crypto(word, alphabet);
+    Interface obj;
+    if (argv[1] == 0)
+    {
+        std::cout << "Входных данных нет";
+        return 0;
+    }
+    std::string word = argv[1];
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz ";
+    int chosenValue;
+    if (CorrectInputData(word, alphabet) == 0)
+    {
+        std::cout << "Некорректный ввод данных. Вводите данные из следующего алфавита - 'abcdefghijklmnopqrstuvwxyz '. ";
+        return 0;
+    }
 
-	chosenValue = obj.ChosenFunction();
+    Crypto crypto(word, alphabet);
 
-	obj.MakeDecision(chosenValue, &crypto);
+    chosenValue = obj.ChosenFunction();
 
-	return 0;
+    obj.MakeDecision(chosenValue, &crypto);
+
+    return 0;
 }
