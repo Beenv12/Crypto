87fff7878443bdfe7686fb13f96b3c4d561c5fe0
Beenv12 <36257286+Beenv12@users.noreply.github.com>
2018-05-22 13:50:46 +0300

    Update crypto.hpp

--- include/crypto.hpp
+++ include/crypto.hpp
@@ -3,30 +3,30 @@
 #include <vector>
 #include <fstream>
 namespace HWCrypto
-	{
-	class Crypto
-	{
-		public:
-		Crypto();
-		Crypto(std::string word, std::string alphabet);
-		void MethodCaesar();
-		void MethodSlogan();
-		void MethodVizhiner();
-		void MethodGamma();
-		void save();
-		void load();
-		~Crypto();
-		private:
-		std::string cryptoword;
-		std::string cryptoalphabet;
-	};
+{
+class Crypto
+{
+  public:
+    Crypto();
+    Crypto(std::string word, std::string alphabet);
+    void MethodCaesar();
+    void MethodSlogan();
+    void MethodVizhiner();
+    void MethodGamma();
+    void save();
+    void load();
+    ~Crypto();
 
-	class Interface
-	{
-		public:
-		int ChosenFunction();
-		void MakeDecision(int chosenValue, Crypto *crypto);
-	};
-	bool  CorrectInputData(std::string word, std::string alphabet);
+  private:
+    std::string cryptoword;
+    std::string cryptoalphabet;
+};
 
+class Interface
+{
+  public:
+    int ChosenFunction();
+    void MakeDecision(int chosenValue, Crypto *crypto);
+};
+bool CorrectInputData(std::string word, std::string alphabet);
 }
